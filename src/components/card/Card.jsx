0ad61a0e4850b3a9a1ca8c4c5c5825b25b1d8658
Madina030596 <didarkhanova.madina@gmail.com>
2024-05-12 17:26:13 +0300

    feat: create favorites slice to add items to favorites

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,17 @@
+// import { useDispatch, useSelector } from "react-redux";
 import s from "./Card.module.css";
 import { CiHeart } from "react-icons/ci";
-// import { FaHeart } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa6";
+import { useActions } from "../../hooks/useActions";
+import { useFavorites } from "../../hooks/useFavorites";
 
 export function Card({ cards }) {
+  const { favorites } = useFavorites();
+
+  const { toggleFavorites } = useActions();
+
+  console.log(favorites);
+
   return (
     <main className={s.container}>
       {cards.map(item => {
@@ -16,8 +25,13 @@ export function Card({ cards }) {
             <p className={`${s.text} ${s.cuisineText}`}>
               Cuisine: {item.cuisine}
             </p>
-            <button>
-              <CiHeart className={s.fav} />
+            {/* <button onClick={() => dispatch(actions.toggleFavorites(item))}> */}
+            <button onClick={() => toggleFavorites(item)}>
+              {favorites.some(r => r.id === item.id) ? (
+                <FaHeart className={s.addedToFav} />
+              ) : (
+                <CiHeart className={s.addToFav} />
+              )}
             </button>
           </div>
         );
--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,9 +1,13 @@
+import { useFavorites } from "../../hooks/useFavorites";
 import s from "./Favorites.module.css";
 
 export function Favorites() {
+  const { favorites } = useFavorites();
+
   return (
     <div>
       <h2 className={s.margin}>Favourites</h2>
+      <span>{favorites.length}</span>
     </div>
   );
 }
